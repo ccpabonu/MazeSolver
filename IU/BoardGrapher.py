1afc5c03199b5ec75758a6f9dc56a98efad25ab2
Christian Pabon <ccpabonu@unal.edu.co>
2022-09-22 22:05:04 -0500

    Add structure and Print Maze

---
+++ IU/BoardGrapher.py
@@ -0,0 +1,38 @@
+import matplotlib.image as image
+import numpy as np
+from PIL import Image
+
+class BoardGrapher:
+
+    def __init__(self, matrix, name, route):
+        self.route = route+"/"
+        self.name = name
+        self.matrix = matrix
+        self.rows, self.columns = matrix.shape
+        self.sPixels = 25
+        self.img = np.ones((self.rows * self.sPixels, self.columns * self.sPixels, 3))
+        self.colorWalls = [0, 0, 0]
+        self.colorTrack = [0, 45, 187]
+        self.printWalls()
+
+    def printWalls(self):
+        for i in range(self.rows):
+            for j in range(self.columns):
+                if self.matrix[i][j] == "w":
+                    self.printCell(i, j, self.colorWalls)
+        self.printImage("Walls")
+
+
+    def printImage(self,i):
+        im = Image.fromarray(np.uint8(self.img * 255))
+        im.save(self.route+f"img/{self.name}-{i}.png")
+
+    def printCell(self, x0, y0, color):
+        for x in range(int(x0)*self.sPixels, int(x0+1)*self.sPixels):
+            for y in range(int(y0)*self.sPixels, int(y0+1)*self.sPixels):
+                self.img[x][y] = np.array(color)
+
+    def printTrack(self, track, i):
+        for item in track :
+            self.printCell(item[0], item[1], self.colorTrack)
+        self.printImage(i)
--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+from IU.BoardGrapher import BoardGrapher
+import csv
+import numpy
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    reader = csv.reader(open("MazeExamples/maze_10x10.csv", "rt"), delimiter=",")
+    x = list(reader)
+    x = [ele for ele in x if ele != []]
+    result = numpy.array(x)
+    route = "IU"
+    g = BoardGrapher(result, "Example2", route)
+    # track = [[0, 1]]
+    # g.printTrack(track, "1")
+    # track = [[0, 1], [1, 1]]
+    # g.printTrack(track, "2")
+    # track = [[0, 1], [1, 1], [2, 1]]
+    # g.printTrack(track, "3")
+    # track = [[0, 1], [1, 1], [2, 1], [2, 2]]
+    # g.printTrack(track, "4")
+    # track = [[0, 1], [1, 1], [2, 1], [2, 2], [2, 3]]
+    # g.printTrack(track, "5")
+    # track = [[0, 1], [1, 1], [2, 1], [2, 2], [2, 3], [3, 3]]
+    # g.printTrack(track, "6")
+    # track = [[0, 1], [1, 1], [2, 1], [2, 2], [2, 3], [3, 3], [4, 3]]
+    # g.printTrack(track, "7")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
