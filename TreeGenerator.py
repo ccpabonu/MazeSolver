f4b7b27f0c234f01fb3c866f1f5279996ff50127
Christian Pabon <ccpabonu@unal.edu.co>
2022-09-25 22:36:05 -0500

    Add Tree Generator

---
+++ TreeGenerator.py
@@ -0,0 +1,39 @@
+from anytree import Node, RenderTree
+from anytree.exporter import DotExporter
+
+
+class TreeGenerator:
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.m, self.n = self.matrix.shape
+        self.root = Node(f"{0},{1}")
+        self.createTree(self.root)
+        print(RenderTree(self.root))
+
+    def createTree(self, root, parent=None):
+        x, y = root.name.split(',')
+        if parent is None:
+            xp, yp = -1, -1
+        else:
+            xp, yp = parent.name.split(',')
+        x, y, xp, yp = int(x), int(y), int(xp), int(yp)
+
+        if y - 1 >= 0:
+            if self.matrix[x][y - 1] == 'c' and (x != xp or y - 1 != yp):
+                child = Node(f"{x},{y - 1}", root)
+                self.createTree(child, root)
+
+        if x - 1 >= 0:
+            if self.matrix[x-1][y] == 'c' and (x - 1 != xp or y != yp):
+                child = Node(f"{x-1},{y}", root)
+                self.createTree(child, root)
+
+        if y + 1 < self.m:
+            if self.matrix[x][y+1] == 'c' and (x != xp or y + 1 != yp):
+                child = Node(f"{x},{y+1}", root)
+                self.createTree(child, root)
+
+        if x + 1 < self.n:
+            if self.matrix[x+1][y] == 'c' and (x + 1 != xp or y != yp):
+                child = Node(f"{x+1},{y}", root)
+                self.createTree(child, root)
